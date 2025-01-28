33dac89315c5e55ad4dd95f2c779d6ec7ad410e5
Mayank Gupta <gmayank9999@gmail.com>
2025-01-28 20:21:45 +0530

    1st pattern

---
+++ 1.cpp
@@ -0,0 +1,22 @@
+// ****
+// ****
+// ****
+// ****
+#include<iostream>
+using namespace std;
+void print1(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    print1(4);
+    return 0;
+}
